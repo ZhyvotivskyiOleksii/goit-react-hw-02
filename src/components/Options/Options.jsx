f69be4272267ece0e775b490cee3c6ffe66c92f1
Oleksii Zhyvotivskyi <v.kubrak@bugsyempire.com>
2025-03-17 13:19:33 +0100

    lesson2

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,14 @@
+const Options = ({ handleFeedback, handleReset, feedbackTypes }) => {
+  return (
+    <div>
+      <button onClick={() => handleFeedback("good")}>Добре</button>
+      <button onClick={() => handleFeedback("neutral")}>Нейтрально</button>
+      <button onClick={() => handleFeedback("bad")}>Погано</button>
+      {(feedbackTypes.good > 0 || feedbackTypes.neutral > 0 || feedbackTypes.bad > 0) && (
+        <button onClick={handleReset}>Reset</button>
+      )}
+    </div>
+  );
+};
+
+export default Options;
